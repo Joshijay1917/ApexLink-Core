7eb1d61722ba1824d5f898af013a97de57f0db8e
Joshijay1917 <jayjoshi1912007@gmail.com>
2026-06-13 14:54:23 +0530

    fix: join room db receiverId fixed

--- src/socket/handlers/join-room.ts
+++ src/socket/handlers/join-room.ts
@@ -20,15 +20,15 @@ export default function(io: Server, socket: Socket) {
         if(userId) {
             const chatExists = await Chat.findOne({ chatId: room })
 
-            if(chatExists) {
-                chatExists.receiverId = userId;
-                await chatExists.save();
-            } else {
+            if(!chatExists) {
                 await Chat.create({
                     chatId: room,
                     senderId: userId,
                     receiverId: userId
                 })
+            } else if(chatExists && chatExists.senderId.toString() !== userId) {
+                chatExists.receiverId = userId;
+                await chatExists.save();
             }
         }
 
